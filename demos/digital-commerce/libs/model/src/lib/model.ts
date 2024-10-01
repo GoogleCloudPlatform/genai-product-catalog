09c64ae002146e30b04638b92331f65dd1aa73d8
Ryan McGuinness <rmcguinness@google.com>
2024-10-01 14:37:48 -0400

    Rolled back model to Flash-001
    Will need to work with the Gemini team to determine why we are getting a
    recursive answer on what works consistently in 001

--- demos/digital-commerce/libs/model/src/lib/model.ts
+++ demos/digital-commerce/libs/model/src/lib/model.ts
@@ -135,7 +135,7 @@ export const ProductAsJsonString = () => {
 
 export const NewGenerativeConfig = (instructions: string) => {
   return {
-    modelName: 'gemini-1.5-flash',
+    modelName: 'gemini-1.5-flash-001',
     genAIToken: '',
     instructions: instructions,
     temperature: 0.2,
